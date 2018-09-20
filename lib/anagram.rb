e20151cbe6ac95573c0b48f2e2b093bffe87750a
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-20 14:08:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  def initialize(input)
+    @letters=input.split('').sort
+  end
+  
+  def match(arr)
+    puts arr
+    arr.select{|word| word.split('').sort==@letters}
+  end
+  
+end
